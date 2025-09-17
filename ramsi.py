586ce1e54565a0c22829024a214282a409e62aab
FalomkinaUFM2025 <veronika.falomkina@gmail.com>
2025-09-17 19:50:28 +0300

    Косметика и общий план ;)

--- ramsi.py
+++ ramsi.py
@@ -42,7 +42,7 @@ emotion_labels = {
     "neutral": "Нейтрально"
 }
 #функция для отображения русского текста
-def put_russian_text(img, text, position, pont_scale, color, thickness):
+def put_russian_text(img, text, position, font_scale, color, thickness):
     #пробуем разные шрифтики :3
     fonts = [
         cv2.FONT_HERSHEY_COMPLEX,
@@ -51,5 +51,92 @@ def put_russian_text(img, text, position, pont_scale, color, thickness):
     ]
 
     #создаем временное изображение для тэкста
-# дз создать презентацию нашему проекту
+    text_img = np.zeros_like(img)
+
+    for font in fonts:
+        try:
+            cv2.putText(text_img, text, position, font, font_scale, color, thickness)
+            #если текст отрисовался не пустой, то сбегаем из цыкла за пирожками
+            if np.max(text_img) > 0:
+                break
+        except:
+            continue
+
+    #если нифига не вышло используем инглиш вери вери гуд аналаги
+    if np.max(text_img) == 0:
+        trans_table = str.maketrans(
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+            "abBrdeex3unKnmHonpcTyoxu4wwib9io0aABBrDEEX3UNKNMHONPCTYOXU4WWIB9IO0A"
+        )
+        text = text.translate(trans_table)
+        cv2.putText(text_img, text, position,  cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness)
+
+    #наклвадываем текст на исходное изоброжение
+    img = cv2.addWeighted(img, 1, text_img, 1, 0)
+    return img
+
+
+print("для закрытия нажмите й в окне с изображением")
+
+# для повышения производительности обрабатываем каждый ный кадр
+frame_counter = 0
+skip_frames = 1 # кажды второй кадр обрабатываем в формалине
+emotion_result = None
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print('ошибка не удалос получит кадр')
+        break
+
+    frame_counter += 1
+
+    # конвертируем в оттенки серого цвета для распознования лиц
+    gray =cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    #ДЕТЕКЦИЯ ЛИЦ НА КАРТИНКЕ
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.1,
+        minNeighbors = 5,
+        minSize = (30,30)
+    )
+
+    # находим все хари на экране и обрабатываем кислотой (;3)
+    for (x, y, w, h) in faces:
+        # рисуем основной прямоугольник
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2)
+
+        # рисуем дополнительные елементы для style
+        corner_lenght = min(w, h) // 4
+        # левый верхний шизоид
+        cv2.line(frame, (x, y), (x + corner_lenght, y), (0, 255, 255), 2)
+        cv2.line(frame, (x, y), (x, y + corner_lenght), (0, 255, 255), 2)
+        # правый верхний шизоид
+        cv2.line(frame, (x + w, y), (x + w - corner_lenght, y), (0, 255, 255), 2)
+        cv2.line(frame, (x + w, y), (x + w + corner_lenght, y), (0, 255, 255), 2)
+        # левый нижний шизоид
+        cv2.line(frame, (x, y + h), (x + corner_lenght, y + h), (0, 255, 255), 2)
+        cv2.line(frame, (x + w, y + h), (x + w, y + h - corner_lenght), (0, 255, 255), 2)
+
+        # распознаем эмоции в каждом N-ном кадрэ
+        if frame_counter % skip_frames == 0:
+            try:
+                # подготавлием личико к распознаванию эмоций(тушь ресничке помадка)
+                face_roi = frame[y:y + h, x:x + w]
+
+                # начищаем морду для модели ксенова
+                face_roi = cv2.resize(face_roi, (224,224)) # размер картнки ожидаемой модели
+                face_roi = cv2.cvtColor(face_roi, cv2.COLOR_BGR2RGB) # модель ажидает rgb
+
+                #нормолизация
+                face_roi = face_roi.astype(np.float32) / 255.0
+
+                # ПРЕОБРАЗУЕМ В ФОРМАТ [1,3,224,224]\
+                input_data = np.expand_dims(face_roi.transpose(2, 0, 1), axis=0)
+
+                # онкс рантайм распознает эиоцию
+                outputs = session.run(None, {input_name: input_data})
+                emotions = outputs[0][0]
+
 
