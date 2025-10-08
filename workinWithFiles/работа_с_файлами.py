a3a16122aa7ed76f2398b76a9b2af8b934d518d1
FalomkinaUFM2025 <veronika.falomkina@gmail.com>
2025-10-08 15:16:35 +0300

    Косметика и общий план ;)

--- workinWithFiles/работа_с_файлами.py
+++ workinWithFiles/работа_с_файлами.py
@@ -2,36 +2,27 @@
 input_file = open('word.in', 'r')
 output_file = open('word.out', 'w')
 
-
 words =  input_file.readline().split()
 # print(lst)
 n = int(words[0])
 k = int(words[1])
 
-def format_essay(n, k, words):
-    line = []
-    char = 0
+line = ''
+char = 0
 
-
-    for word in words:
-        if char + len(word) + (1 if line else 0) <= k:
-            line.append(word)
-            char += len(word) + (1 if line else 0)
-        else:
-            # Word doesn't fit, so print the current line and start a new one
-            print(" ".join(line))
-            line = [word]
-            char = len(word)
-
-
-    with open("word.out", "w") as f:
-            # Redirect standard output to the file
-            import sys
-            old_stdout = sys.stdout
-            sys.stdout = f
-
-    format_essay(n, k, input_file)
-
+for word in words:
+     with open('word.in', 'r', encoding='utf-8') as f:
+        file_content = f.read()  # Читаем все содержимое файла в одну строку
+        # Удаляем все пробелы из строки
+        content_without_spaces = file_content.replace(' ','')
+    # Подсчитываем количество оставшихся символов
+        char = len(content_without_spaces)
+        if char <= 100:
+           output_file.write(input_file.read())
+# print(f"Количество символов без пробелов: {char}")
+# в переменную char записываем ею саму + len(word)
+#проверяем чиcло символов в строке если
+# оно меньше или равно к то мы берем переменную line и line + переменную word + пробел
 
 
 output_file.close()
