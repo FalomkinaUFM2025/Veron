4c4be37efa11c3146b366b5227d62cc0e7bd7cfc
FalomkinaUFM2025 <veronika.falomkina@gmail.com>
2025-10-12 16:25:53 +0300

    Косметика и общий план ;)

--- workinWithFiles/musorka.py
+++ workinWithFiles/musorka.py
@@ -1,10 +1,23 @@
-n = ''
-a = " ia u mami prograMMER "
-for i in a:
-    if i == " ":
-        pass
-    else:
-        n += i
-print(n)
-print(1 + 1)
-print('1' + '1')
+#В консоли
+input_file = open('word.in', 'r')
+output_file = open('word.out', 'w')
+
+words =  input_file.readline().split()
+# print(lst)
+n = 12
+k = 14
+
+line = ''
+char = 0
+
+for word in words:
+
+   if char + len(words) <= n:
+      line = line + word + ' '
+      char = char + len(words)
+   else:
+      line = word + ' '
+      char = len(words)
+   output_file.write(line[:-1] + '\n')
+output_file.close()
+input_file.close()
--- workinWithFiles/работа_с_файлами.py
+++ workinWithFiles/работа_с_файлами.py
@@ -11,19 +11,13 @@ line = ''
 char = 0
 
 for word in words:
-     with open('word.in', 'r', encoding='utf-8') as f:
-        file_content = f.read()  # Читаем все содержимое файла в одну строку
-        # Удаляем все пробелы из строки
-        content_without_spaces = file_content.replace(' ','')
-    # Подсчитываем количество оставшихся символов
-        char = len(content_without_spaces)
-        if char <= 100:
-           output_file.write(input_file.read())
-# print(f"Количество символов без пробелов: {char}")
-# в переменную char записываем ею саму + len(word)
-#проверяем чиcло символов в строке если
-# оно меньше или равно к то мы берем переменную line и line + переменную word + пробел
 
-
+   if char + len(words) <= n:
+      line = line + word + ' '
+      char = char + len(words)
+   else:
+      line = word + ' '
+      char = len(words)
+   output_file.write(line[:-1] + '\n')
 output_file.close()
 input_file.close()
